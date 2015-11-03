a06785a238130b219a420e7369166f7debfd0373
mihosasaki <miho.sprit@gmail.com>
2015-11-03 12:35:09 +0000

    message...

---
+++ index.js
@@ -0,0 +1,6 @@
+$('#open').click(function(){
+      $('#drawer ul').toggleClass('showMenue');
+    });
+    $('#drawer ul li').click(function(){
+      $('#drawer ul').toggleClass('showMenue');
+    });
