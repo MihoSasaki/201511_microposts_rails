4b59f75d2df19ba4fce3c6b8692ec850ee994cf4
mihosasaki <miho.sprit@gmail.com>
2015-11-24 15:14:40 +0000

    add login and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :crate, :destroy]
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
