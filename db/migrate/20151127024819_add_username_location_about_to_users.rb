e3274a3c0cad9aeb78fd71845b0e6cbcde460ab1
mihosasaki <miho.sprit@gmail.com>
2015-12-01 04:13:32 +0000

    Profile-updated2

--- db/migrate/20151127024819_add_username_location_about_to_users.rb
+++ db/migrate/20151127024819_add_username_location_about_to_users.rb
@@ -1,7 +1,8 @@
-class AddUsernameLocationAboutToUsers < ActiveRecord::Migration
+class AddUsernameLocationEmailAboutToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
     add_column :users, :location, :string
+    add_column :users, :email, :string
     add_column :users, :about, :text
   end
 end
