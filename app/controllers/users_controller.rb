30aeace28857b26b0fc0f7f0051e22fbadb5f269
mihosasaki <miho.sprit@gmail.com>
2015-12-05 05:36:22 +0000

    posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 before_action :set_user, :correct_user, only:[:edit, :update]
 
   def show
-  
+  @user = User.find(params[:id])
+  @microposts = @user.microposts.order(created_at: :desc)
   end
   def new
     @user = User.new
